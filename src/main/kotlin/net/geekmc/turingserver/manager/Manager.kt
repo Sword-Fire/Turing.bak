0a48d3a76b24d2638803630820df5eae41d98080
Anzide <geekmc2022@gmail.com>
2022-07-19 00:14:22 +0800

    Create Manager.kt

---
+++ src/main/kotlin/net/geekmc/turingserver/manager/Manager.kt
@@ -0,0 +1,25 @@
+package net.geekmc.turingserver.manager
+
+import net.minestom.server.MinecraftServer
+import net.minestom.server.event.Event
+import net.minestom.server.event.EventNode
+import net.minestom.server.event.GlobalEventHandler
+import net.minestom.server.instance.InstanceManager
+import net.minestom.server.monitoring.BenchmarkManager
+import net.minestom.server.timer.SchedulerManager
+
+object Manager {
+
+    val globalNode: GlobalEventHandler
+        get() = MinecraftServer.getGlobalEventHandler()
+
+    val scheduler: SchedulerManager
+        get() = MinecraftServer.getSchedulerManager()
+
+    val instance: InstanceManager
+        get() = MinecraftServer.getInstanceManager()
+
+    val benchmark: BenchmarkManager
+        get() = MinecraftServer.getBenchmarkManager()
+
+}
